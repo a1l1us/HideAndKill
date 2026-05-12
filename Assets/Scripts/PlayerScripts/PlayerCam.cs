d4d76b5971dc959e94a66f6c61d52ba3d2bbc018
a1l1us <translucentailius@gmail.com>
2026-05-11 22:51:56 -0400

    Working on player input system

--- Assets/Scripts/PlayerScripts/PlayerCam.cs
+++ Assets/Scripts/PlayerScripts/PlayerCam.cs
@@ -1,8 +1,31 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerCam : MonoBehaviour
 {
     //Provided by this tutorial https://www.youtube.com/watch?v=f473C43s8nE
+    //Made changes using the new Input system rather than the legacy one using this: https://www.youtube.com/watch?v=lclDl-NGUMg&t=209s
+
+    [Header("Input Action Asset")]
+    [SerializeField] private InputActionAsset playerControls;
+
+    [Header("Action Map Name References")]
+    [SerializeField] private string actionMapName = "Player";
+
+    [Header("Action Name References")]
+    [SerializeField] private string move = "Move";
+    [SerializeField] private string look = "Look";
+    [SerializeField] private string jump = "Jump";
+    [SerializeField] private string sprint = "Sprint";
+
+    private InputAction moveAction;
+    private InputAction lookAction;
+    private InputAction jumpAction;
+    private InputAction sprintAction;
+
+    
+
+    [Header("Sensitivity")]
     public float sensX;
     public float sensY;
 
@@ -11,12 +34,10 @@ public class PlayerCam : MonoBehaviour
     float xRotation;
     float yRotation;
 
-    
-
     void Start()
     {
         //Puts the cursor at the center of the screen and lock it there.
-        Cursor.lockState = CursorLockMode.locked;
+        Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
